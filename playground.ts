96dddf0fb52222af87d1ee7b3fdec6b80f019ae4
Aniket Biprojit Chowdhury <8.aniket.chowdhury@gmail.com>
2023-11-02 12:03:03 +0530

    feat: playground

--- playground.ts
+++ playground.ts
@@ -1,14 +1,14 @@
-import { initialize, configure, textToText } from "./dist";
+import { initialize, configure, textToText } from "sdk";
 import { config } from "dotenv";
 
 config({});
 
 const auth = initialize({
-  meity_config_url: process.env.BHASHINI_MEITY_CONFIG_URL,
-  dhruva_compute_url: process.env.BHASHINI_DHRUVA_COMPUTE_URL,
-  user_id: process.env.BHASHINI_USER_ID,
-  api_key: process.env.BHASHINI_API_KEY,
-  pipeline_id: process.env.BHASHINI_PIPELINE_ID,
+  meity_config_url: process.env.BHASHINI_MEITY_CONFIG_URL!,
+  dhruva_compute_url: process.env.BHASHINI_DHRUVA_COMPUTE_URL!,
+  user_id: process.env.BHASHINI_USER_ID!,
+  api_key: process.env.BHASHINI_API_KEY!,
+  pipeline_id: process.env.BHASHINI_PIPELINE_ID!,
 });
 const run = async () => {
   console.log(
